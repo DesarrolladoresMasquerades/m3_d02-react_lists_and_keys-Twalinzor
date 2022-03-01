87cbe2b81a7dbc0a0ff83774ca39caf843034970
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-03-01 15:46:57 +0100

    Class

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,25 @@
+import movies from "../movieData.json";
+import { useState } from "react";
+import MovieCard from "./MovieCard";
+
+export default function MoviesList() {
+  const [moviesList, setMoviesList] = useState(movies);
+
+  function deleteMovieById(id){
+    /* This .filter() copies all movies that are NOT being deleted*/
+    setMoviesList(moviesList.filter((movie) => movie._id!==id))
+  }
+
+  return (
+    <div>
+      <h1>Movies form the json file</h1>
+      {moviesList.map((movie) => (
+        <MovieCard 
+        key={movie._id} 
+        movie={movie}
+        deleteMovieById={deleteMovieById}
+        />
+      ))}
+    </div>
+  );
+}
